7c14d8be44ca2a83eadc8d923a6075f8531ebfee
CuriosBasant <basantbrpl@gmail.com>
2024-06-13 20:18:59 +0530

    refactor: ⚰️ remove dead code

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,32 +1,16 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Text, View } from 'react-native'
+import { Link, Stack } from 'expo-router'
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+      <View className='flex-1 items-center justify-center p-6'>
+        <Text className='text-3xl font-bold'>This screen doesn't exist.</Text>
+        <Link href='/' className='text-base text-primary'>
+          Go to home screen!
         </Link>
-      </ThemedView>
+      </View>
     </>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
