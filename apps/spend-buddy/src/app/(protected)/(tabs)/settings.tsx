f23e41afcafae2184b358f6f9bef5741cb67b3da
CuriosBasant <basantbrpl@gmail.com>
2024-06-21 20:40:47 +0530

    feat: ✨ add `UpdateButton`

--- apps/spend-buddy/src/app/(protected)/(tabs)/settings.tsx
+++ apps/spend-buddy/src/app/(protected)/(tabs)/settings.tsx
@@ -1,11 +1,16 @@
+import { checkForUpdateAsync, fetchUpdateAsync, reloadAsync, useUpdates } from 'expo-updates'
+
+import { tuplifyPromise } from '@my/lib/utils'
+
 import { SettingItem } from '~/components/setting-item'
-import { Screen } from '~/components/ui'
+import { Screen, Toast } from '~/components/ui'
 import { useLogout } from '~/features/auth'
 
 export default function SettingsScreen() {
   return (
     <Screen className='gap-6 px-0' scrollable>
       <SettingItem iconName='palette' label='Change Theme' to='/settings/change-theme' />
+      <AppUpdateButton />
       <LogoutButton />
     </Screen>
   )
@@ -14,4 +19,36 @@ export default function SettingsScreen() {
 function LogoutButton() {
   const { mutate } = useLogout()
   return <SettingItem iconName='sign-out-alt' label='Logout' onPress={() => mutate()} />
+}
+
+function AppUpdateButton() {
+  const { isChecking, isDownloading, isUpdateAvailable, isUpdatePending } = useUpdates()
+
+  return isChecking ? (
+    <SettingItem iconName='download' label='Checking for updates...' disabled />
+  ) : isUpdateAvailable ? (
+    <SettingItem
+      iconName='download'
+      label='Download Update'
+      onPress={async () => {
+        const [error, result] = await tuplifyPromise(fetchUpdateAsync)()
+        if (error) return Toast.error('Updates are not supported in the current environment.')
+        if (result.isNew) Toast.show('Downloading the update!')
+      }}
+    />
+  ) : isDownloading ? (
+    <SettingItem iconName='download' label='Downloading new updates...' disabled />
+  ) : isUpdatePending ? (
+    <SettingItem iconName='download' label='Reload to update' onPress={reloadAsync} />
+  ) : (
+    <SettingItem
+      iconName='download'
+      label='Check for updates'
+      onPress={async () => {
+        const [error, result] = await tuplifyPromise(checkForUpdateAsync)()
+        if (error) return Toast.error('Updates are not supported in the current environment.')
+        if (!result.isAvailable) Toast.show('There are no new updates available!')
+      }}
+    />
+  )
 }
