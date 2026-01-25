3e2c4d27db143b89b409f0c48cc335397047f3d6
curiosbasant <basantbrpl@gmail.com>
2026-01-25 20:10:36 +0530

    feat(parinaam): 🐛 resolve `missing-suspense-with-csr-bailout`

--- apps/basant.dev/src/app/s/parinaam/(page)/layout.tsx
+++ apps/basant.dev/src/app/s/parinaam/(page)/layout.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react'
+
 export default function ParinaamPageLayout(props: LayoutProps<'/s/parinaam'>) {
   return (
     <div className='space-y-16'>
       {props.form}
       {props.prefetchResults}
-      {props.children}
+      <Suspense>{props.children}</Suspense>
     </div>
   )
 }
