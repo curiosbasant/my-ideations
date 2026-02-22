2287bc5ffef447fc55c9069cab48e8c0de88b0e8
curiosbasant <basantbrpl@gmail.com>
2026-02-22 12:59:10 +0530

    style(sdbms): 💄 add root loading spinner

--- apps/basant.dev/src/app/s/sdbms/loading.tsx
+++ apps/basant.dev/src/app/s/sdbms/loading.tsx
@@ -1,3 +1,9 @@
-export default function Loading() {
-  return <div>Loading</div>
+import { LoaderCircleIcon } from 'lucide-react'
+
+export default function LoadingPage() {
+  return (
+    <div className='flex size-full items-center justify-center'>
+      <LoaderCircleIcon className='size-8 shrink-0 animate-spin md:size-10' />
+    </div>
+  )
 }
