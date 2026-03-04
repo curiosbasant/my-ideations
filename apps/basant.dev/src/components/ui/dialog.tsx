c0b0c2fca5799075e6ef59cb5ff343acdf6e11a5
curiosbasant <basantbrpl@gmail.com>
2026-03-05 00:19:24 +0530

    style: 💄 make `dialog` scrollable

--- apps/basant.dev/src/components/ui/dialog.tsx
+++ apps/basant.dev/src/components/ui/dialog.tsx
@@ -41,7 +41,9 @@ export function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot='dialog-overlay'
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 backdrop-blur-2xs fixed inset-0 z-50 bg-black/50',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 backdrop-blur-2xs bg-background/50',
+        'fixed inset-0 z-50',
+        'scrollbar-hidden grid place-items-center overflow-y-auto sm:p-4',
         className,
       )}
       {...props}
@@ -63,26 +65,27 @@ export function DialogContent({
 }) {
   return (
     <DialogPortal data-slot='dialog-portal'>
-      <DialogOverlay />
-      <DialogPrimitive.Content
-        data-slot='dialog-content'
-        className={cn(
-          'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed start-1/2 top-1/2 z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg',
-          className,
-        )}
-        onEscapeKeyDown={closeOnEscapeKeyDown ? undefined : (ev) => ev.preventDefault()}
-        onInteractOutside={closeOnInteractOutside ? undefined : (ev) => ev.preventDefault()}
-        {...props}>
-        {children}
-        {showCloseButton && (
-          <DialogPrimitive.Close
-            data-slot='dialog-close'
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-xs focus:outline-hidden absolute end-4 top-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0">
-            <XIcon />
-            <span className='sr-only'>Close</span>
-          </DialogPrimitive.Close>
-        )}
-      </DialogPrimitive.Content>
+      <DialogOverlay>
+        <DialogPrimitive.Content
+          data-slot='dialog-content'
+          className={cn(
+            'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 relative z-50 grid w-full gap-4 p-6 shadow-lg duration-200 sm:max-w-lg sm:rounded-lg sm:border',
+            className,
+          )}
+          onEscapeKeyDown={closeOnEscapeKeyDown ? undefined : (ev) => ev.preventDefault()}
+          onInteractOutside={closeOnInteractOutside ? undefined : (ev) => ev.preventDefault()}
+          {...props}>
+          {children}
+          {showCloseButton && (
+            <DialogPrimitive.Close
+              data-slot='dialog-close'
+              className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-xs focus:outline-hidden absolute end-4 top-4 opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0">
+              <XIcon />
+              <span className='sr-only'>Close</span>
+            </DialogPrimitive.Close>
+          )}
+        </DialogPrimitive.Content>
+      </DialogOverlay>
     </DialogPortal>
   )
 }
@@ -124,7 +127,7 @@ export function DialogDescription({
   return (
     <DialogPrimitive.Description
       data-slot='dialog-description'
-      className={cn('text-muted-foreground text-sm', className)}
+      className={cn('text-muted-foreground text-balance text-sm', className)}
       {...props}
     />
   )
