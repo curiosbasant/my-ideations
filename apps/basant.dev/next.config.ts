4e7777d3aa905ab68047eaa593abedea89c0155a
curiosbasant <basantbrpl@gmail.com>
2026-03-08 00:13:30 +0530

    chore: 💩 clean up

--- apps/basant.dev/next.config.ts
+++ apps/basant.dev/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from 'next'
 
-const LOCAL_ROOT_HOSTNAME = process.env.NEXT_PUBLIC_LOCAL_ROOT_HOSTNAME
-
 export default {
   allowedDevOrigins: getDevOrigins(),
   cacheComponents: true,
@@ -20,6 +18,7 @@ function getDevOrigins() {
   if (process.env.NODE_ENV === 'production') return void 0
 
   const allowedDevOrigins = []
+  const LOCAL_ROOT_HOSTNAME = process.env.NEXT_PUBLIC_LOCAL_ROOT_HOSTNAME
   if (LOCAL_ROOT_HOSTNAME) allowedDevOrigins.push(LOCAL_ROOT_HOSTNAME, `*.${LOCAL_ROOT_HOSTNAME}`)
   if (process.env.DEV_ORIGIN) allowedDevOrigins.push(process.env.DEV_ORIGIN)
 
