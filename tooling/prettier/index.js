9e4285a8965dfffebbba279881d7ec6cff14f5c9
curiosbasant <basantbrpl@gmail.com>
2026-03-08 01:33:43 +0530

    chore: ⬆️ upgrade dependencies

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -45,7 +45,7 @@ const config = {
   importOrderMergeDuplicateImports: true,
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
-  importOrderTypeScriptVersion: '5.8.3',
+  importOrderTypeScriptVersion: '5.9.3',
 
   overrides: [
     {
